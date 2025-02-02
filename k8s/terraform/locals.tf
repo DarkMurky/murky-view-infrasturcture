bb3b5d29f8e7a889021832feb1370ad680fad1d2
Karolis <karolisvaitiekunas@protonmail.com>
2025-02-02 17:44:37 +0200

    ci: update eks region

--- k8s/terraform/locals.tf
+++ k8s/terraform/locals.tf
@@ -1,8 +1,8 @@
 locals {
   env         = var.environment
-  region      = "eu-north-1"
-  zone1       = "eu-north-1a"
-  zone2       = "eu-north-1b"
+  region      = "eu-central-1"
+  zone1       = "eu-central-1a"
+  zone2       = "eu-central-1b"
   eks_name    = "lens-view"
   eks_version = "1.30"
 }
